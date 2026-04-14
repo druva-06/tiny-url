7405faf8087e8f72c8a6b038cb84d58b973bc23d
Druvasena Reddy Naini <nainidruvasenareddy3737@gmail.com>
2026-04-14 23:37:20 +0530

    initial commit

---
+++ cmd/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	r := gin.Default()
+
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "Welcome, Tiny Url",
+		})
+	})
+
+	r.Run(":8080")
+
+}
